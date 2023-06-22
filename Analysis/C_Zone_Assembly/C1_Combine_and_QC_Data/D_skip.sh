f0a4d25d38572b73a50cac0c99a938ae0fe9c3c9
ssander5 <ss93@iu.edu>
2023-06-22 11:38:06 -0400

    Update D_skip.sh

--- Analysis/C_Zone_Assembly/C1_Combine_and_QC_Data/D_skip.sh
+++ Analysis/C_Zone_Assembly/C1_Combine_and_QC_Data/D_skip.sh
@@ -1,3 +1,4 @@
+  GNU nano 6.2                                                                                         D_skip.sh *                                                                                                 
 #!/bin/bash
 
 #$ -M sheri.anne.sanders@gmail.com
@@ -13,10 +14,16 @@ else
     mkdir bbmap
 fi
 
+for z in ../C0_*/zone*; do
+    mkdir ./bbmap/${z#../C0_*/}
+done
+
+
 echo "SKIPPING Removing phix adaptors and sequencing artifacts using BBMAP"
 
 for g in ./sickle/*/*trimclean.sickleclean.fq ; do
     o=${g#./sickle*/}
+    echo $o
 
     cp ${g} ./bbmap/${o%trim*}trimclean.sickleclean.spikeclean.fq
 done
